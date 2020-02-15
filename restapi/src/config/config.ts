de90cd18d8b8508333e2421358f67c0a9ef3013c
Fano Yong <fanoyong@fanoyong.com>
2020-02-15 15:57:18 -0500

    re-structure folders

---
+++ restapi/src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramdev",
+    "password": "test1234",
+    "database": "udagramdev",
+    "host": "http://udagramdev.cctxbq835mzq.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1d",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-media-bucket-fano"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
